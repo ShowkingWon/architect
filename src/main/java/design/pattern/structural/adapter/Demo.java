c5e5453f7afe4448636988047b5369f11b19de88
ShowKingWon <wanxiujin123@163.com>
2021-01-11 00:25:59 +0800

    design patterns

---
+++ src/main/java/design/pattern/structural/adapter/Demo.java
@@ -0,0 +1,10 @@
+package design.pattern.structural.adapter;
+
+/**
+ * @author JerryWan
+ * @descripion TODO
+ * @date 2021-01-11  0:12
+ */
+public class Demo {
+    // 适配器模式
+}
---
+++ src/main/java/design/pattern/structural/composite/Demo.java
@@ -0,0 +1,10 @@
+package design.pattern.structural.composite;
+
+/**
+ * @author JerryWan
+ * @descripion TODO
+ * @date 2021-01-11  0:12
+ */
+public class Demo {
+    // bridge
+}
